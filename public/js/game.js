15b97790fa962ff61197a215fb98b02f44f63038
Fabiano Beselga <fabianobeselga@gmail.com>
2010-11-18 02:04:52 -0200

    js OO beginning hooray

--- public/js/game.js
+++ public/js/game.js
@@ -1,14 +1,111 @@
+/* INVADER */
+function Invader(left,top) {
+  this.left = left;
+  this.top = top;
+
+  this.draw = function(left,top) {
+    $("<div class='invader' style='left:" + left +"px;top:" + top + "'></div>").appendTo("#space");
+  }
+
+  this.shoot = function() {
+    $('<div class="invaderBullet" style="top:' + this.top + 'px;left:' + this.left + 'px"></div>').appendTo("#space");
+    $('.invaderBullet').animate({
+        top: '650px'
+    }, 6000);
+  }
+
+  //this.move = function() {
+      //this.animate({
+        //left: '+=10' 
+      //}, 50);
+  //}
+
+  this.draw(left,top);
+}
+/* INVADER */
+
+/* AIRCRAFT */
+function Aircraft() {
+  addMovementListener();
+  addShootListener();
+  this.draw = function() {
+    var canvas=document.getElementById("aircraft");
+    var c=canvas.getContext("2d");
+   
+    c.moveTo(0,60);
+    c.lineTo(50,0); // line 1
+    c.lineTo(100,60); // line 2
+   
+    c.lineWidth=10;
+    c.lineJoin="round";
+    c.strokeStyle= "#e325cc"; // line color
+    c.stroke();	
+  }
+
+  this.shoot = function() {
+    $('<div class="bullet" style="top:' + ($("#aircraft").position().top) + 'px;left:' + ($("#aircraft").position().left + 45) + 'px"></div>').appendTo("#space");
+    $('.bullet').animate({
+        top: '-10'
+    }, 4000, function() { 
+      $(this).remove();
+    });
+  }
+
+  function addShootListener() {
+    $(document).keydown(function(event) {
+        if (event.keyCode == 32) {
+          air.shoot();
+        }
+    });
+  }
+
+  function addMovementListener() {
+    $(document).keypress(function(event) {
+      switch(event.keyCode)
+      {
+          case 37: //left
+            moveLeft();
+            break;
+          case 38: //up
+            $("#aircraft").animate({
+              top: '-=10' 
+            }, 10);
+            break;
+          case 39: //right
+            moveRight();
+            break;
+          case 40: //down
+            $("#aircraft").animate({
+              top: '+=10' 
+            }, 10);
+            break;
+      }
+    });
+  }
+}
+/* AIRCRAFT */
+
+/* GLOBAL VARS */
 var score = 0;
+var invaders = 1;
+var air = new Aircraft();
+/* GLOBAL VARS */
+
+/* MAIN */
 $(document).ready(function() {
-  drawAircraft();
-  window.addEventListener("keydown", getKey, false);
+  air.draw();
   setInterval("detect();",10);
-  var invaders = setInterval("moveInvadersRight();",500);
-  var invadersShoots = setInterval("invadersShoot();",3000);
-  setTimeout("invadersClean();",60000);
+
+  for (i=0;i<=invaders;i++) {
+    var inv = new Invader(Math.random()*1000,Math.random()*500);
+    inv.shoot();
+  }
   setInterval("cleanInvadersBullets();",10000);
 });
+/* MAIN */
 
+
+/* ANONYMOUS METHODS */
 function cleanInvadersBullets() {
   $('.invaderBullet').fadeOut("slow");
 }
@@ -24,13 +121,13 @@ function updateScore(value) {
 }
 
 function observeDone() {
-  if ($('.invader').length == 0) {
-    clearInterval(invaders);
-    //clearInterval(invadersShoots);
-    alert("/stage clear! you can get some extra points shooting the reds coming from space");
-    //if (window.location.match(/play/) ) {
-    window.location = "/stages/2";
-    //}
+  if ($('.invader').length == 1) {
+    invaders = invaders * 2;
+    for (i=0;i<=invaders;i++) {
+      var inv = new Invader(Math.random()*1000,Math.random()*500);
+      //inv.move();
+      inv.shoot();
+    }
   }
 }
 
@@ -44,52 +141,26 @@ function detect() {
     updateScore(10);
     observeDone();
   });
-  $('.bullet').collidesWith('.invaderBullet').each(function(inv) {
+
+  $('.bullet').collidesWith('.invaderBullet').each(function() {
       var invbul = $(this);
-      $(invbul).fadeOut("slow", function() { invbul.remove(); } );
+      $(invbul).collidesWith('.bullet').each(function() {
+        invbul.remove();
+        $(this).remove();
+      });
       updateScore(20);
   });
+
   $('.invaderBullet').collidesWith('#aircraft').each(function(inv) {
       var air = $(this);
-      //$(air).fadeOut("slow",  function() {
-      //  $(air).remove();
-      //});
-
-      $(air).remove();
+      air.fadeOut("slow",  function() {
+        air.remove();
+      });
       alert("game over!");
       window.location = "/";
   });
 }
 
-function moveInvadersRight() {
-  $('#invaders').animate({
-    left: '+=10' 
-  }, 50);
-}
-
-function drawAircraft() {
-  var canvas=document.getElementById("aircraft");
-	var c=canvas.getContext("2d");
- 
-	c.moveTo(0,60);
-	c.lineTo(50,0); // line 1
-	c.lineTo(100,60); // line 2
- 
-	c.lineWidth=10;
-	c.lineJoin="round";
-	c.strokeStyle= "#e325cc"; // line color
-	c.stroke();	
-}
-
-function drawCurveNave() {
-  var canvas=document.getElementById("aircraft");
-	var c=canvas.getContext("2d");
-  c.moveTo(0,60);
-	c.quadraticCurveTo(50, 0, 100, 60);
-	c.lineWidth=10;
-	c.strokeStyle= "#e325cc"; // line color
-	c.stroke();	
-}
 
 function moveRight() {
   $("#aircraft").animate({
@@ -103,54 +174,5 @@ function moveLeft() {
   }, 10);
 }
 
-function invadersShoot() {
-  $('<div class="invaderBullet" style="top:100px;left:' + (Math.random() * 1000) + 'px"></div>').appendTo("#space");
-  $('.invaderBullet').animate({
-      top: '650px'
-  }, 6000);
 
-}
-
-function shoot() {
-  $('<div class="bullet" style="top:' + ($("#aircraft").position().top) + 'px;left:' + ($("#aircraft").position().left + 45) + 'px"></div>').appendTo("#space");
-  $('.bullet').animate({
-      top: '-10'
-  }, 4000, function() { 
-    $(this).remove();
-  });
-}
-
-function getKey() {
-    if (event == null) {
-        keyCode = window.event.keyCode;
-        window.event.preventDefault();
-    }
-    else {
-        keyCode = event.keyCode;
-        event.preventDefault();
-    }
-
-    switch(keyCode)
-    {
-        case 32: //space bar
-          shoot();
-          break;
-        case 37: //left
-          moveLeft();
-          break;
-        case 38: //up
-          $("#aircraft").animate({
-            top: '-=10' 
-          }, 10);
-          break;
-        case 39: //right
-          moveRight();
-          break;
-        case 40: //down
-          $("#aircraft").animate({
-            top: '+=10' 
-          }, 10);
-          break;
-    }
-}
 
