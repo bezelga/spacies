6119c6c698d14238fbeeabcef581b2ccc9b3b32c
Fabiano Beselga <fabianobeselga@gmail.com>
2010-11-19 01:22:53 -0200

    old school env

--- spacies.rb
+++ spacies.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 #db
 #require 'mongo_mapper'
+
 #template
 require 'haml'
 set :haml, :format => :html5
@@ -20,10 +21,10 @@ set :haml, :format => :html5
   #MongoMapper.connect(:production)
 #end
 
-class Aircraft
+#class Aircraft
   #include MongoMapper::Document
   #key :name, String, :required => true
-end
+#end
 
 get '/' do
   haml :index
@@ -33,17 +34,6 @@ get '/play' do
   haml :play 
 end
 
-get '/stages/2' do
-  haml :stage2
-end
-
-
-get '/aircrafts' do
-  #@aircrafts = Aircraft.all
-  haml :aircrafts
-end
-
-get '/aircrafts/:name' do
-  #Aircraft.create(:name => params[:name])
-  redirect '/aircrafts'
+get '/old_school' do
+  haml :old_school
 end
