f164529dafb10036131771b3126aa7d793ab4c8e
Philip Sampaio <philip.sampaio@gmail.com>
2014-01-29 00:08:13 -0200

    Updated gems and renamed javascript folder

--- spacies.rb
+++ spacies.rb
@@ -1,37 +1,15 @@
 require 'rubygems'
 require 'sinatra'
-#db
-#require 'mongo_mapper'
 
-#template
 require 'haml'
 set :haml, :format => :html5
-
-#configure do
-  #MongoMapper.config = {
-    #:development => { 'uri' => 'mongodb://localhost/spacies' },
-  #}
-#end
-
-#configure :development do
-  #MongoMapper.connect(:development)
-#end
-
-#configure :production do
-  #MongoMapper.connect(:production)
-#end
-
-#class Aircraft
-  #include MongoMapper::Document
-  #key :name, String, :required => true
-#end
 
 get '/' do
   haml :index
 end
 
 get '/play' do
-  haml :play 
+  haml :play
 end
 
 get '/old_school' do
